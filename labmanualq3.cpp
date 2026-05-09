d03514df48a40774f67c416487f0e7ec03bc551e
Yaksh Yadav <yaksh25csu267@ncuindia.edu>
2026-05-09 18:23:01 +0530

    commit

---
+++ labmanualq3.cpp
@@ -0,0 +1,12 @@
+/* An architect wants to calculate the space covered by a circular fountain. 
+Implement a solution to compute the area of a circle.*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    float radius;
+    cout<<"Enter the radius: "<<endl;
+    cin>> radius;
+    cout<<"The area of the circular fountain is: "<<3.14*radius*radius<<endl;
+    return 0;
+}
